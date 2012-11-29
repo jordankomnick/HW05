7fc59ff3f851798eda59aae5b1be52f33b371a1b
Jordan Komnick <jordan@komnick.com>
2012-11-28 23:11:41 -0500

    wrote some matrix graph stuff

--- vc10/MatrixGraph.cpp
+++ vc10/MatrixGraph.cpp
@@ -1,20 +1,34 @@
 
 #include "MatrixGraph.h"
 
-MatrixGraph::MatrixGraph(unsigned num_nodes){
-
+MatrixGraph::MatrixGraph(unsigned num_nodes)
+{
+	M = std::vector<std::vector<EdgeWeight>>(num_nodes);
+	for(int i = 0; i < num_nodes; i++)
+	{
+		M.push_back(std::vector<EdgeWeight>(num_nodes));
+		for(int j = 0; j < num_nodes; j++)
+		{
+			M[i][j] = 0;
+		}
+	}
+	num_edges = 0;
 }
 
 MatrixGraph::~MatrixGraph(){ //may not need destructors, because you should only delete things YOU made using new
 
 }
 
-void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
-
+void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight)
+{
+	M[u][v] = weight;
 }
 
-EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const{
-	return NULL;
+EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const
+{
+	if(M[u][v] > 0)
+		return M[u][v];
+	return 0;
 }
 
 std::list<NWPair> MatrixGraph::getAdj(NodeID u) const{
